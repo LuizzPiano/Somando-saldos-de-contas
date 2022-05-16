768b449481e150d45e8db05a74121bc4d22e5358
LuizzPiano <luizgustavo.cavalcante01@gmail.com>
2022-05-15 23:25:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-nzirnu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-nzirnu)
---
+++ index.js
@@ -0,0 +1,6 @@
+const saldoContas = [100,200,40,1.9,300,1000,9.99];
+
+const total = saldoContas.reduce( (acumulador, valor) => acumulador + valor);
+
+console.log(total);
+
